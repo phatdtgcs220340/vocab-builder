7608c13ecf98fd880c7d69edf8149e9bc963db89
Phat Do <phatdtgcs220340@fpt.edu.vn>
2024-11-19 00:45:18 +0700

    dotenv setup

--- client/src/main.js
+++ client/src/main.js
@@ -1,9 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import './assets/style.css'
+import dotenv from 'dotenv'
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+dotenv.config()
