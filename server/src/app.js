d6b095b1bf7c5928cbf1e590f0ac91781c6b2006
Phat Do <phatdtgcs220340@fpt.edu.vn>
2024-11-19 01:33:52 +0700

    refactor routes

--- server/src/app.js
+++ server/src/app.js
@@ -18,6 +18,7 @@ app.use(cors({
 
 app.use('/api', router);
 app.use('/api', authenticateRouter)
+
 mongoose.set('strictQuery', true);
 mongoose.connect(process.env.MONGODB_URI)
     .then(() => console.log('Vocab Builder DB Connected'))
