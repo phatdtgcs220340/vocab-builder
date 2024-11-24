32eb53779ecb64828b023186d32172c37e262e0e
Phat Do <phatdtgcs220340@fpt.edu.vn>
2024-11-24 22:46:59 +0700

    handle update and find by id api

--- client/src/helpers/helpers.js
+++ client/src/helpers/helpers.js
@@ -1,37 +1,48 @@
 import axios from 'axios'
-import {LOGIN_API, PING_API, SIGN_UP_API} from "@/helpers/api";
+import {LOGIN_API, PING_API, SIGN_UP_API, VOCAB_API} from "@/helpers/api";
+
+const handleForbiddenError = fn => (...params) =>
+    fn(...params).catch(error => {
+        if (error?.status === 403) {
+            window.location.href = '/welcome'
+        }
+        return error
+    })
 
 const handleError = fn => (...params) =>
-    fn(...params).catch(error => console.error(error))
+    fn(...params).catch(error =>  error)
 
 export const api = {
     login : handleError(async payload => {
-        try {
-            const res = await axios.post(LOGIN_API, payload)
-            return res.data
-        } catch (error) {
-            return error
-        }
+        const res = await axios.post(LOGIN_API, payload)
+        return res.data
     }),
     signUp : handleError(async payload => {
-        try {
-            const res = await axios.post(SIGN_UP_API, payload)
-            return res.data
-        } catch (error) {
-            return error
-        }
+        const res = await axios.post(SIGN_UP_API, payload)
+        return res.data
     }),
     authenticate: handleError(async (token) => {
-        try {
-            const response = await axios.get(PING_API, {
-                headers: {
-                    Authorization: `Bearer ${token}`
-                }
-            });
-            return response.data;
-        } catch (error) {
-            return error.status
-        }
-    })
+        const response = await axios.get(PING_API, authConfig(token))
+        return response.data
+    }),
+    findAllWords: handleForbiddenError(async (token) => {
+        const response = await axios.get(VOCAB_API, authConfig(token))
+        return response.data
+    }),
+    findWordById: handleForbiddenError(async (token, id) => {
+        const response = await axios.get(`${VOCAB_API}/${id}`, authConfig(token))
+        return response.data
+    }),
+    updateWord: handleForbiddenError(async (token, id, payload) => {
+        const response = await axios.put(`${VOCAB_API}/${id}`, payload, authConfig(token))
+        return response.data
+})
+}
 
+const authConfig = token => {
+    return {
+        headers : {
+            Authorization : `Bearer ${token}`
+        }
+    }
 }
