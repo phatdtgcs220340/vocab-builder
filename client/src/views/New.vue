e52c5a762f28a151a73ec7a05889878217143258
Phat Do <phatdtgcs220340@fpt.edu.vn>
2024-11-27 00:25:10 +0700

    pop out message in page new

--- client/src/views/New.vue
+++ client/src/views/New.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
     <word-form @createOrUpdate="submit"></word-form>
+    <div class="fixed inset-0 flex justify-center mt-20 z-50 h-fit">
+      <div v-if="messagePopout" class="w-fit py-2 px-5 font-semibold" :class="response.status">
+        {{ response.text }}
+      </div>
+    </div>
   </div>
 </template>
 
@@ -10,12 +15,38 @@
 
   export default {
     name: "NewComponent",
+    data() {
+      return {
+        response : {
+          text : '',
+          status : ''
+        },
+        messagePopout : false
+      }
+    },
     components: {
       WordForm
     },
     methods: {
       async submit(form) {
+        this.response = {}
         const response = await api.createWord(window.localStorage.getItem("vocab-access-token"), form)
+
+        this.messagePopout = true
+        if (response.name !== "AxiosError") {
+          this.response = {
+            text: "✅ New word created",
+            status: "bg-green-400 text-green-700"
+          }
+        } else {
+          this.response = {
+            text: "Fail to create word",
+            status: "bg-red-400 text-red-700"
+          }
+        }
+        setTimeout(() => {
+          this.messagePopout = false
+        }, 2000)
       }
     }
   }
