a93d12a9fc790ba793be37beda27d3d2ae83a91e
Phat Do <phatdtgcs220340@fpt.edu.vn>
2024-11-27 00:25:28 +0700

    updated route

--- server/api/routes/vocab-builder.route.js
+++ server/api/routes/vocab-builder.route.js
@@ -4,7 +4,7 @@ import {validateAccessToken} from "../middlewares/authenticate.middleware.js";
 
 const router = express.Router()
 
-router.post('/create-a-word', validateAccessToken, controller.create_a_word)
+router.post('/', validateAccessToken, controller.create_a_word)
 router.get('/', validateAccessToken, controller.find_all_words)
 router.get('/:id', validateAccessToken, controller.find_a_word_by_id)
 router.put('/:id', validateAccessToken, controller.update_a_word)
