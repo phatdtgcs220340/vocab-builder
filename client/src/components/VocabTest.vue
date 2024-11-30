a66ab98fcbb8cea43e1f42d2932de2b80e1b2d0c
Phat Do <phat.do@knorex.com>
2024-11-30 22:07:30 +0700

    test feature

--- client/src/components/VocabTest.vue
+++ client/src/components/VocabTest.vue
@@ -1,12 +1,53 @@
 <template>
-  
+  <div class="flex flex-col gap-5 mt-5">
+    <div class="flex justify-between gap-20" v-for="(word, index) in words" :key="index">
+      <div>
+      {{ word.english }}
+      </div>
+      <div>
+        <input v-model="answerList[index]" class="border border-gray-300 focus:outline-none px-2 py-1 rounded-sm" placeholder="Enter german word...">
+      </div>
+    </div>
+    <div v-if="submitted" class="font-medium text-red-500">Score: {{ score }} out of 10</div>
+    <button @click="submit" class="px-8 py-2 bg-white border-2 rounded-sm border-gray-700 font-medium hover:bg-gray-700 hover:text-white">Submit</button>
+    <button v-if="submitted" @click="startOver" class="px-8 py-2 bg-white border-2 rounded-sm border-gray-700 font-medium hover:bg-gray-700 hover:text-white">Start over</button>
+  </div>
 </template>
 
 <script>
 export default {
   name: "VocabTest",
+  props : {
+    words : {
+      type : Array,
+      required : true
+    }
+  },
+  data() {
+    return {
+      score : 0,
+      submitted : false,
+      answerList : []
+    }
+  },
   components: {
 
+  },
+  methods : {
+    submit() {
+      this.score = 0
+      this.submitted = true
+      for (let i = 0; i < this.answerList.length; i++) {
+        if (this.answerList[i] === this.words[i].german) {
+          ++this.score;
+        }
+      }
+    },
+    startOver() {
+      this.submitted = false;
+      this.score = 0;
+      this.answerList = []
+    }
   }
 }
 </script>
--- client/src/views/Test.vue
+++ client/src/views/Test.vue
@@ -1,14 +1,29 @@
 <template>
-  <div>
-
+  <div class="flex flex-col items-center">
+    <div class="text-4xl mt-12">
+      Test
+    </div>
+    <vocab-test :words="words"/>
   </div>
 </template>
 
 <script>
+import VocabTest from "@/components/VocabTest.vue";
+import {api} from "@/helpers/helpers";
+
 export default {
   name: "TestComponent",
   components: {
-
+    VocabTest
+  },
+  data() {
+    return {
+      words : []
+    }
+  },
+  async mounted() {
+    const fetchedWords =  await api.findAllWords(window.localStorage.getItem("vocab-access-token"), 1)
+    this.words = fetchedWords.data
   }
 }
 </script>
